74b1c1a9c44148387f0cff913e87e7489e3dda46
George Campbell <campbell.goe@gmail.com>
2024-06-13 15:23:03 +0100

    add text logo

---
+++ components/icons/textLogo.tsx
@@ -0,0 +1,5 @@
+export default function LogoIcon(props: React.ComponentProps<'img'>) {
+  const src = '/text-ninja-shanti.png';
+  const alt = 'Ninja Shanti';
+  return <img src={src} alt={alt} />;
+}
