78b493ee671ea6e1158eda6bb82f26272d983333
George Campbell <campbell.goe@gmail.com>
2024-06-14 12:15:31 +0100

    update package.json can't get lock becaue wont install loading ages will restart

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -54,6 +54,7 @@ const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
   : '';
 const endpoint = `${domain}${SHOPIFY_GRAPHQL_API_ENDPOINT}`;
+console.log('endpoint:', endpoint);
 const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!;
 
 type ExtractVariables<T> = T extends { variables: object } ? T['variables'] : never;
